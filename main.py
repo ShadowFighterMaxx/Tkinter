20bcd180b45cd6acaf837c10c696dabb748e5242
ShadowFighterMaxx <1annavarapu.sridatta@gmail.com>
2024-09-16 11:04:42 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+root = Tk()
+root.title("New Window")
+root.geometry("400x400")
+root.config(background="red")
+
+l1 = Label(root ,pady = 10, text = "Welcome To Python Tkinter", bg = "white", fg = "blue")
+l2 = Label(root ,pady = 10, text = "Enter Your Name", bg = "white", fg = "blue")
+l1.pack()
+l2.pack()
+e1 = Entry(root)
+e1.pack()
+def display():
+    name = e1.get()
+    t1 = "Welcome "+ name
+    l3 = Label(root, text = t1)
+    l3 .pack()
+b1 = Button(root, text = "Show", command = display)
+b1.pack()
+root.mainloop()
