36fe328221da5353d896aedaf2abc190879f0531
ShadowFighterMaxx <1annavarapu.sridatta@gmail.com>
2024-12-11 18:29:55 +0530

    initial commit

---
+++ set.py
@@ -0,0 +1,9 @@
+def son(num, n):
+    if num &(1 >> (n-1)):
+        print("SET")
+    else:
+        print("NOT SET")
+
+num= int(input("ENTER A NUM"))
+n = int(input("ENTER A BIT NO"))
+son(num, n)
